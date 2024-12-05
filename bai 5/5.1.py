e05a98bd0e45ded17005946d0ea3aab238905059
LeCongManhTung <lecongmanhtung3000@gmail.com>
2024-12-05 15:06:19 +0700

    Add files via upload

---
+++ bai 5/5.1.py
@@ -0,0 +1,11 @@
+print ("le cong manh tung")
+print ("235752021610041")
+import mymath # Note no .py
+values = [2,4,6,8,10]
+print('Squares:')
+for v in values:
+ print(mymath.square(v))
+print('Cubes:')
+for v in values:
+ print(mymath.cube(v))
+print('Average: ' + str(mymath.average(values)))
---
+++ bai 5/5.3.py
@@ -0,0 +1,9 @@
+
+print ("le cong manh tung")
+print ("235752021610041")
+
+array = list(range(12, 38))
+print("Mảng ban đầu:", array)
+reversed_array = array[::-1]
+print("Mảng đã đảo ngược:", reversed_array)
+
