9b613a4c5d2751bd596604df6c1db31a7a511a73
LeCongManhTung <lecongmanhtung3000@gmail.com>
2024-12-05 15:12:06 +0700

    Delete bai1 directory

--- bai1/2.2.py
+++
@@ -1,8 +0,0 @@
-print('sinh vien: le cong manh tung')
-print('mssv: 235752021610041' )
-print('##########')
-n=int(input("Enter a number --- >"))
-if n%2 == 0:
-    print ("EVEN Number") ;
-else :
-    print ("ODD Number");
